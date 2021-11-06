cef751e5cda50401c9c76cb7a1c4797a8b591147
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-11-06 19:39:45 +0300

    Fix naming

--- src/screens/MainWindow/DropdownButton/DropdownButton.js
+++ src/screens/MainWindow/DropdownButton/DropdownButton.js
@@ -25,12 +25,12 @@ export default function DropdownButton({rates, setCurrencyToDisplay}) {
             {loading && <p>Loading ...</p>}
             {error && <p>Something went wrong ...</p>}
             {symbols && 
-                <div className={styles.container}>
-                    <button className={styles.click} onClick={() => handleOpenDropdown()}>Select currency</button>
-                    <div className={!open ? styles.list : styles.newList}>
+                <div className={styles.dropdown}>
+                    <button className={styles.dropdown__click} onClick={() => handleOpenDropdown()}>Select currency</button>
+                    <div className={!open ? styles.dropdown__list : styles.dropdown__newList}>
                         {Object.entries(symbols).map(([key, value]) => {
                             return (
-                                <button key={key} className={styles.rates} onClick={() => handleSelectCurrency(key, value)}>{value}</button>
+                                <button key={key} className={styles.dropdown__rates} onClick={() => handleSelectCurrency(key, value)}>{value}</button>
                             )
                         })
                         }
